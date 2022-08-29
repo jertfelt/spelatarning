e762cba772f976a378562fd6c7ba67933481fbee
Tova Jertfelt <tovizitan@gmail.com>
2022-08-29 15:20:51 +0200

    Create ToDoForm.js

---
+++ src/ToDoForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+const ToDoForm = ({ addTask }) => {
+
+    const [ userInput, setUserInput ] = useState("");
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask(userInput);
+        setUserInput("");
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={userInput} 
+            type="text" 
+            onChange={handleChange} 
+            placeholder="Uppgift"/>
+            <button
+            className="knapp">Lägg till</button>
+        </form>
+    );
+};
+
+export default ToDoForm;
