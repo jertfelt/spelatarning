1a43a1d9ed11b1d17554a03d6b580e0ba463ed63
Tova Jertfelt <tovizitan@gmail.com>
2022-08-30 11:32:27 +0200

    Create TodoList.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+import ToDo from './Todo';
+
+const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
+    return (
+        <div className="padding">
+            {toDoList.map(todo => {
+                return (
+                    <ToDo todo={todo} 
+                    handleToggle={handleToggle} 
+                    handleFilter={handleFilter}/>
+                )
+            })}
+            <button 
+            className="todo__clear"
+            style={{margin: '10px'}
+            } 
+            onClick={handleFilter}>
+              Rensa färdiga
+            </button>
+        </div>
+    );
+};
+
+export default ToDoList;
