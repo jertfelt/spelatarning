c1e2d199297354a7eb31a05e6172e1ea6dbf6c54
Tova Jertfelt <tovizitan@gmail.com>
2022-08-30 12:41:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import Dice from "./views/Dice";
 import Header from "./Header";
 import Home from "./views/Home";
-
+import Kontakt from "./views/Kontakt";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom" 
 
 function App() {
   
 
   return (
+    <Router>
     <div className="App">
       <Header />
-      <div className="content__main">
-        <Home />
+      <Routes>
+        <Route path="/spela-tarning" element={<Dice/>}
+        ></Route>
+        <Route path="/kontakt"
+        element={<Kontakt/>}>
+        </Route>
+        <Route exact path="/" 
+         element={<Home/>}
+        >
+          
+        </Route>
+      </Routes>
+     
+      
+       
       </div>
-      </div>
-
+    
+      </Router>
   );
 }
 
